c8f092d02e1febf17bac2ef313139454da1bfeea
Pedro <pedrolucasgoncalvespedro@gmail.com>
2025-06-04 09:08:59 -0300

    feature: add jwt configuration on swagger

---
+++ src/main/java/com/decoticket/api/config/JwtConfig.java
@@ -0,0 +1,23 @@
+package com.decoticket.api.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+public class JwtConfig {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Bean
+    public JwtDecoder jwtDecoder(){
+        SecretKeySpec key = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+        return NimbusJwtDecoder.withSecretKey(key).build();
+    }
+}
